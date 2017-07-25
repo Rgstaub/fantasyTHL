b1950c69f93171a7031e97ce3f5fa0c1fb06dc39
Rgstaub <rgstaub@gmail.com>
2017-07-25 12:10:49 -0400

    ...

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-// Google Spreadsheets API key:  AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc
 
-// test URL: https://sheets.googleapis.com/v4/spreadsheets/1azyTuUPH2BxNwoKlkHp2HkJEbspIKgKugirm8V4A0cM?key=AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc
 
 const apiKey = "AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc";
 
+let spreadsheetObj = {};
+
 let spreadsheetID = "1azyTuUPH2BxNwoKlkHp2HkJEbspIKgKugirm8V4A0cM"
 
-function testAPI() {
+let getSpreadsheet  = function() {
     let queryURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}?key=${apiKey}`;
 
     $.ajax({
         url: queryURL,
-        method: "GET",
-        key: apiKey
+        method: "GET"
     })
 
     .done(function(data) {
         console.log(data);
+        spreadsheetObj = data;
     })
 }
+
