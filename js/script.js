5ec52795bb814c0e154c6d5475eac3689ed06f06
Rgstaub <rgstaub@gmail.com>
2017-07-25 11:58:22 -0400

    got the basic API call working

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 // Google Spreadsheets API key:  AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc
 
+// test URL: https://sheets.googleapis.com/v4/spreadsheets/1azyTuUPH2BxNwoKlkHp2HkJEbspIKgKugirm8V4A0cM?key=AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc
+
 const apiKey = "AIzaSyAil2fLaeuR0RTx_pJOzBtghvqE856E_Bc";
 
 let spreadsheetID = "1azyTuUPH2BxNwoKlkHp2HkJEbspIKgKugirm8V4A0cM"
 
 function testAPI() {
-    let queryURL = "https://sheets.googleapis.com/v4/spreadsheets/1azyTuUPH2BxNwoKlkHp2HkJEbspIKgKugirm8V4A0cM";
+    let queryURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}?key=${apiKey}`;
 
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+        key: apiKey
     })
 
     .done(function(data) {
